fe7199aee7524d9b51d219680108825c49bed4dc
hlnikniky <orekhovs09@gmail.com>
2026-07-07 16:13:22 +0700

    init

---
+++ src/main/java/net/hlnikniky/factoryexpansion/block/BasicDrillBlock.java
@@ -0,0 +1,9 @@
+package net.hlnikniky.factoryexpansion.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class BasicDrillBlock extends Block {
+    public BasicDrillBlock(Properties properties) {
+        super(properties);
+    }
+}
